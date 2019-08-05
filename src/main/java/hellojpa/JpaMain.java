deee7d43d6cbaf16ca3fc93d7b413c19f513d8b8
kwanghyunMoon <mgh3326@naver.com>
2019-08-05 16:22:16 +0900

    update

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
   public static void main(String[] args) {
@@ -12,9 +13,16 @@ public class JpaMain {
     EntityTransaction transaction = entityManager.getTransaction();
     transaction.begin();
     try {
-      Member member = entityManager.find(Member.class, 1L);
-      member.setName("HelloA");
-      entityManager.persist(member);
+//      Member member = entityManager.find(Member.class, 1L);
+//      member.setName("HelloJPA");
+//      entityManager.persist(member);
+      List<Member> resultList = entityManager.createQuery("select  m from Member as m", Member.class)
+              .setFirstResult(1)
+              .setMaxResults(10)
+              .getResultList();
+      for (Member member : resultList) {
+        System.out.println("member.name = " + member.getName());
+      }
       transaction.commit();
     } catch (Exception e) {
       transaction.rollback();
