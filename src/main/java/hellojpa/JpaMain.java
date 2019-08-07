575cc16d971c9d26f7ca27247dde6d38cd831cf8
kwanghyunMoon <mgh3326@naver.com>
2019-08-07 19:42:34 +0900

    update 다양한 연관관계 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,15 +9,34 @@ import java.util.List;
 public class JpaMain {
   public static void main(String[] args) {
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
-    EntityManager entityManager = entityManagerFactory.createEntityManager();
-    EntityTransaction transaction = entityManager.getTransaction();
+    EntityManager em = entityManagerFactory.createEntityManager();
+    EntityTransaction transaction = em.getTransaction();
     transaction.begin();
     try {
+//팀 저장
+      Team team = new Team();
+      team.setName("TeamA");
+      em.persist(team);
+//회원 저장
+      Member member = new Member();
+      member.setName("member1");
+      member.changeTeam(team); //단방향 연관관계 설정, 참조 저장
+      em.persist(member);
+team.addMember(member);
+
+      em.flush();
+      em.clear();
+
+      Team findTeam = em.find(Team.class, team.getId());
+      List<Member> members = findTeam.getMembers();
+      for (Member m : members) {
+        System.out.println("m : " + m.getName());
+      }
       transaction.commit();
     } catch (Exception e) {
       transaction.rollback();
     } finally {
-      entityManager.close();
+      em.close();
     }
     entityManagerFactory.close();
   }
