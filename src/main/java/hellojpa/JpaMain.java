9b150d5085c6a84a8a18341e49ca02cf9dbe28b9
kwanghyunMoon <mgh3326@naver.com>
2019-08-08 15:17:53 +0900

    update 고급매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -13,25 +14,15 @@ public class JpaMain {
     EntityTransaction transaction = em.getTransaction();
     transaction.begin();
     try {
-//팀 저장
-      Team team = new Team();
-      team.setName("TeamA");
-      em.persist(team);
-//회원 저장
       Member member = new Member();
-      member.setName("member1");
-      member.changeTeam(team); //단방향 연관관계 설정, 참조 저장
-      em.persist(member);
-team.addMember(member);
+      member.setName("kwanghyun");
 
+      member.setCreateBy("Kim");
+      member.setCreatedDate(LocalDateTime.now());
+      em.persist(member);
       em.flush();
       em.clear();
 
-      Team findTeam = em.find(Team.class, team.getId());
-      List<Member> members = findTeam.getMembers();
-      for (Member m : members) {
-        System.out.println("m : " + m.getName());
-      }
       transaction.commit();
     } catch (Exception e) {
       transaction.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.concurrent.locks.Lock;
 
 @Entity
-public class Member {
+public class Member  extends BaseEntity {
   @Id
   @GeneratedValue
   private Long id;
